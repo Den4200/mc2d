638c9a189013b0645b892a1e10743f76a477cb02
Den4200 <dpham.42@hotmail.com>
2020-04-11 13:29:29 -0400

    added basic logic for random chunk map generation

---
+++ mc2d/core/map_generator.py
@@ -0,0 +1,42 @@
+import random
+
+
+class MapGenerator:
+
+    def __init__(self):
+        self.map = None
+
+    def generate_chunk(self, size_x, size_y):
+        self.map = [[0 for _ in range(size_x)]for i in range(size_y)]
+
+        # Set the bottom floor
+        for x in range(size_x):
+        	self.map[-1][x] = 1
+
+        layer_size = size_x
+        for y in range(size_y - 1):
+        	if layer_size == 0:
+        		break
+
+        	layer_size = random.randrange(0, layer_size)
+        	layer_left_pos = random.randint(0, size_x - layer_size)
+
+        	for x in range(layer_size):
+
+        		# Check if a block is underneath
+        		if self.map[y - 1][x + layer_left_pos] == 1:
+
+        			# Check to see if the block is not on the edges
+        			if layer_left_pos + x < size_x - 1 and layer_left_pos + x != 0:
+
+        				# Check if there is a block diagonally down left and right
+        				if self.map[y - 1][x + layer_left_pos - 1] == 1 and self.map[y - 1][x + layer_left_pos + 1] == 1:
+	        				self.map[y][x + layer_left_pos] = 1
+
+        self.map[:-1] = list(reversed(self.map[:-1]))
+
+
+if __name__ == '__main__':
+    map_gen = MapGenerator()
+    map_gen.generate_chunk(8, 3)
+    print(map_gen.map)
