f7759e8e706400a70a30e8ea093227420e3b6c54
Den4200 <dpham.42@hotmail.com>
2020-04-11 14:18:56 -0400

    [Map Generation] Chunks can be added to the left or right

--- mc2d/core/map_generator.py
+++ mc2d/core/map_generator.py
@@ -4,18 +4,18 @@ import random
 class MapGenerator:
 
     def __init__(self):
-        self.map = None
+        self.chunks = list()
 
         self.DIRT_ID = 1
         self.GRASS_ID = 2
 
-    def generate_chunk(self, size_x, size_y):
-        self.map = [[0 for _ in range(size_x)]for i in range(size_y)]
+    def generate_chunk(self, size_x, size_y, side):
+        chunk = [[0 for _ in range(size_x)]for i in range(size_y)]
 
         # Set the bottom floor
         for y in range(1, 5):
             for x in range(size_x):
-                self.map[-y][x] = 1
+                chunk[-y][x] = 1
 
         layer_size = size_x
         for y in range(size_y - 1):
@@ -28,39 +28,53 @@ class MapGenerator:
             for x in range(layer_size):
 
                 # Check if a block is underneath
-                if self.map[y - 1][x + layer_left_pos] == self.DIRT_ID:
+                if chunk[y - 1][x + layer_left_pos] == self.DIRT_ID:
 
                     # Check to see if the block is not on the edges
                     if layer_left_pos + x < size_x - 1 and layer_left_pos + x != 0:
 
                         # Check if there is a block diagonally down left and right
                         if (
-                            self.map[y - 1][x + layer_left_pos - 1] == self.DIRT_ID and
-                            self.map[y - 1][x + layer_left_pos + 1] == self.DIRT_ID
+                            chunk[y - 1][x + layer_left_pos - 1] == self.DIRT_ID and
+                            chunk[y - 1][x + layer_left_pos + 1] == self.DIRT_ID
                         ):
-                            self.map[y][x + layer_left_pos] = self.DIRT_ID
+                            chunk[y][x + layer_left_pos] = self.DIRT_ID
 
-        self.map[:-4] = list(reversed(self.map[:-4]))
+        chunk[:-4] = list(reversed(chunk[:-4]))
 
         grass_count = 0
+        run = True
         for y in range(size_y):
-            for x in range(size_x):
-                
-                if grass_count == size_x:
-                    return
 
-                if self.map[y][x] != 0:
+            if not run:
+                break
+
+            for x in range(size_x):
+
+                if grass_count == size_x:
+                    run = False
+                    break
+
+                if chunk[y][x] != 0:
 
                     if y == 0:
-                        self.map[y][x] = self.GRASS_ID
+                        chunk[y][x] = self.GRASS_ID
                         grass_count += 1
 
-                    elif self.map[y - 1][x] not in (self.DIRT_ID, self.GRASS_ID):
-                        self.map[y][x] = self.GRASS_ID
+                    # Check to make sure there is not grass or dirt above
+                    elif chunk[y - 1][x] not in (self.DIRT_ID, self.GRASS_ID):
+                        chunk[y][x] = self.GRASS_ID
                         grass_count += 1
+
+        if side == 'left':
+            self.chunks.insert(0, chunk)
+        elif side == 'right':
+            self.chunks.append(chunk)
+
+        return chunk
 
 
 if __name__ == '__main__':
     map_gen = MapGenerator()
-    map_gen.generate_chunk(8, 8)
-    print(map_gen.map)
+    chunk = map_gen.generate_chunk(8, 8, 'right')
+    print(chunk)
